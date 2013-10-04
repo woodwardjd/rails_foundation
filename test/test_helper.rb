1a76b5f562d41d241bbe4912cc6a35c8e3c9b77b
Jared Beck <jared@singlebrook.com>
2013-10-04 15:26:54 -0400

    adds first test, to demonstrate factory usage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,15 +18,8 @@ Spork.prefork do
   MiniTest::Reporters.use! MiniTest::Reporters::ProgressReporter.new
 
   class ActiveSupport::TestCase
+    include FactoryGirl::Syntax::Methods
     ActiveRecord::Migration.check_pending!
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    #
-    # Note: You'll currently still have to declare fixtures explicitly in integration tests
-    # -- they do not yet inherit this setting
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 
 end
