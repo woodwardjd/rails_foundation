b11d90ddfbf3394c483759085685f6b58cb7f71f
Leon Miller-Out <leon@singlebrook.com>
2013-10-07 13:52:12 -0400

    Leverage FactoryGirl's abbreviated syntax

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 describe User do
   it "has a name" do
-    FactoryGirl.build(:user).name.must_equal "John Doe"
+    build(:user).name.must_equal "John Doe"
   end
 
   it "can be stubbed with mocha" do
