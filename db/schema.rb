628a88a7f90e32e2d979945f68a0001a3c0a21c2
Jared Beck <jared@singlebrook.com>
2013-10-07 19:48:36 -0400

    adds devise lockable module

--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,15 @@ ActiveRecord::Schema.define(version: 20131007202152) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end
