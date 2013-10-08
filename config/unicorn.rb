9bf958cc6e7b02308978cb088ec4e1355083aea2
Leon Miller-Out <leon@singlebrook.com>
2013-10-08 14:40:38 -0400

    Make conditionals more readable

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,8 +9,9 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
+  end
 end
 
 after_fork do |server, worker|
@@ -19,6 +20,7 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
+  end
 end
